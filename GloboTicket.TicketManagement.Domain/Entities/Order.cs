aad72272164042d89a3a47fc1106076b647e7b70
Silviu <silviu.catrina@gmail.com>
2021-02-18 19:17:14 +0200

    Add project files.

---
+++ GloboTicket.TicketManagement.Domain/Entities/Order.cs
@@ -0,0 +1,15 @@
+﻿using GloboTicket.TicketManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Domain.Entities
+{
+    class Order: AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
