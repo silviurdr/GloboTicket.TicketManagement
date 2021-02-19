a21128a53cde9542cd6b04ba0dce27c5681677dd
Silviu <silviu.catrina@gmail.com>
2021-02-19 21:00:07 +0200

    Finish section 5: Creating the Infrastructure Project

--- GloboTicket.TicketManagement.Application/Contracts/Persistence/IOrderRepository.cs
+++ GloboTicket.TicketManagement.Application/Contracts/Persistence/IOrderRepository.cs
@@ -2,10 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GloboTicket.TicketManagement.Application.Contracts.Persistence
 {
     public interface IOrderRepository: IAsyncRepository<Order>
     {
+        Task<List<Order>> GetPagedOrdersForMonth(DateTime date, int page, int size);
+        Task<int> GetTotalCountOfOrdersForMonth(DateTime date);
     }
 }
--- GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
 using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Application.Models.Mail;
 using GloboTicket.TicketManagement.Domain.Entities;
 using MediatR;
 using System;
@@ -14,11 +16,14 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
     {
         private readonly IMapper _mapper;
         private readonly IEventRepository _eventRepository;
+        private readonly IEmailService _emailService;
 
-        public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository)
+        public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository,
+            IEmailService emailService)
         {
             _mapper = mapper;
             _eventRepository = eventRepository;
+            _emailService = emailService;
         }
         public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
         {
@@ -33,6 +38,17 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
 
             var @event = _mapper.Map<Event>(request);
             @event = await _eventRepository.AddAsync(@event);
+
+            var email = new Email() { To = "silviu.catrina@gmail.com", Body = $"A new event was created: {request}", Subject = "A new event was created" };
+
+            try
+            {
+                await _emailService.SendEmail(email);
+            }
+            catch (Exception ex)
+            {
+                //this shouldn't stop the API from doing else so this can be logged
+            }
 
             return @event.EventId;
         }
